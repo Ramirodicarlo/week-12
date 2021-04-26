da8f4c70a76dfcd89f52bf622acc12fbc8ff8b47
Ramiro Di Carlo <ramirodicarlo1@gmail.com>
2021-04-26 18:28:30 -0300

    Finished 2 and 3

--- operations.test.js
+++ operations.test.js
@@ -2,14 +2,59 @@ import { expect } from "@jest/globals";
 import * as operations from "./operations";
 
 var op = operations;
+//1
 
 test("Enter text and return the same text",() =>{
     expect(op.texto("house")).toBe("house")
 });
+test("Enter text and return the same text",() =>{
+    expect(op.texto("1234")).toBe("1234")
+});
 
-test("Sum 1 and 2 = 3",() =>{
+//2
+
+test("Sum of 1 and 2 = 3",() =>{
     expect(op.suma(1,2)).toBe(3)
 });
-test("Sum a and b = ab",() =>{
+test("Sum of 1 and -2 = -1",() =>{
+    expect(op.suma(1,-2)).toBe(-1)
+});
+test("Sum of 1.54 and 2 = 3.54",() =>{
+    expect(op.suma(1.54,2)).toBe(3.54)
+});
+test("Sum of a and b = ab",() =>{
     expect(op.suma("a","b")).toBe("ab")
 });
+test("Sum of null and 1 = 1",() =>{
+    expect(op.suma(null,1)).toBe(1)
+})
+test("Sum of 1,2,\"a\" and 1 =  1,2,\"a1\"",() =>{
+    var arr = [1,2,"a"];
+    var arr2 = [1,2,"a1"];
+    expect(op.suma(arr,1)).toBe(arr2)
+});
+
+//3
+test("Subtraction of 2 and 1 = 1",() =>{
+    expect(op.resta(2,1)).toBe(1)
+});
+test("Subtraction of -1 and 2 = -3",() =>{
+    expect(op.suma(-1,2)).toBe(-3)
+});
+test("Sustraction of 2 and 1.54 = 0.46",() =>{
+    expect(op.suma(2,1.54)).toBe(0.46)
+});
+test("Sustraction of a and b = NaN",() =>{
+    expect(op.suma("a","b")).toBe("NaN")
+});
+test("Sustraction of null and 1 = -1",() =>{
+    expect(op.suma(null,1)).toBe(-1)
+});
+test("Sustraction of 1 and null = 1",() =>{
+    expect(op.suma(1,null)).toBe(1)
+});
+test("Sustraction of 1,2,\"a\" and 1 =  1,2,\"a-1\"",() =>{
+    var arr = [1,2,"a"];
+    expect(op.suma(arr,1)).toBe(NaN)
+});
+//3
---
+++ prueba.js
@@ -0,0 +1,4 @@
+var arr = [1,2,"a"];
+var ss = 1;
+var casa = arr - ss;
+console.log(casa);
